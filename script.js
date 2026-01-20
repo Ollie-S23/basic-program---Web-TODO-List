999f654952bccb1218372cd12cef48767b5b9054
Oliver Scott <olliechina1516@gmail.com>
2026-01-20 15:37:27 +1100

    update: allow clear and delete buttons

--- script.js
+++ script.js
@@ -7,8 +7,20 @@ submitButton.addEventListener("click", function() {
     if (newEntry.trim() !== "") {
         let entryParagraph = document.createElement("p");
         entryParagraph.textContent = newEntry;
-        todoEntries.appendChild(entryParagraph); 
+        todoEntries.appendChild(entryParagraph);
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        entryParagraph.appendChild(deleteButton);
+        deleteButton.addEventListener("click", function() {
+            todoEntries.removeChild(entryParagraph);
+        });
+
     }
     todoInput.value = "";
 });
 
+let clearButton = document.getElementById("clear_button");
+
+clearButton.addEventListener("click", function() {
+    todoEntries.innerHTML = "";
+});
